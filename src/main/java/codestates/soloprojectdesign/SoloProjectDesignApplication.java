ce4f441c3b66b828cc6649de0c06232c413ff70e
sungmingt <qotjdals8313@naver.com>
2022-08-16 14:13:09 +0900

    first commit

---
+++ src/main/java/codestates/soloprojectdesign/SoloProjectDesignApplication.java
@@ -0,0 +1,13 @@
+package codestates.soloprojectdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProjectDesignApplication.class, args);
+	}
+
+}
